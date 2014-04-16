7f8b43caa6a587898eb031f1d4ad5dd66c3c7f5f
Mark Yarmoshuk <mark.yarmoshuk@gmail.com>
2014-04-16 09:29:45 -0400

    geo files, app/template files added

---
+++ app/templates/footer.php
@@ -0,0 +1,12 @@
+<div class="siv-footer">
+	<h5 class="siv-foot-copy">&copy; UNHCR <span><?php echo(date('Y')); ?></span></h5>
+	<ul class="siv-inline-list">
+		<li><a id="terms-link" class="siv-inline-link" href="#" data-html="/data/notices/terms.html">Terms &amp; Conditions</a></li>
+		<li>|</li>
+		<li><a id="contact-link" class="siv-inline-link" href="#" data-html="/data/notices/contact.html">Contact Us</a></li>
+		<li>|</li>
+		<li><a id="jira-feedback-reporter" class="siv-footer-link">Feedback</a></li>
+		<li>|</li>
+		<li><a id="jira-bug-reporter" class="siv-footer-link" href="#">Report a Bug</a></li>
+	</ul>
+</div>
